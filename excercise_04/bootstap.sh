722ad7eaf196660ecde1a42610bf06ad6ff82410
Hemanth B <hemanth22hemu@gmail.com>
2026-01-26 22:45:48 +0530

    Replace SSH setup with Nginx and Python installation

--- excercise_04/bootstap.sh
+++ excercise_04/bootstap.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-# Enable ssh password authentication
-echo "[TASK 1] Enable ssh password authentication"
-sed -i 's/^#PasswordAuthentication yes/PasswordAuthentication yes/' /etc/ssh/sshd_config
-sed -i 's/^#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
-systemctl reload sshd
-
-# Set Root password
-echo "[TASK 2] Set root password"
-echo -e "hemanth\nhemanth" | passwd root >/dev/null 2>&1
+echo "[TASK 1] Install nginx"
+dnf install nginx -y
+echo "[TASK 2] enable and start nginx service"
+systemctl enable nginx
+systemctl start nginx
+echo "[TASK 3] Check status of nginx service"
+systemctl status nginx --no-pager
+echo "[TASK 4] Install python3.12"
+dnf install python3.12 -y
